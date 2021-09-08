f6af47efe7c12e69d8d566e74a20240510b2f7a2
Ivan Fokeev <vanya@vanya.cc>
2021-09-08 10:20:32 +0300

    useless

--- services/postgraphile/src/utils/basicAuth.js
+++
@@ -1,7 +0,0 @@
-const { N8N_BASIC_AUTH } = process.env;
-
-const getBasicAuthHeaders = () => ({
-  'Authorization': `Basic ${new Buffer(N8N_BASIC_AUTH).toString('base64')}`
-})
-
-export default getBasicAuthHeaders;
--- services/postgraphile/src/utils/sortBy.js
+++
@@ -1,3 +0,0 @@
-export const sortBy = (arr, field) => {
-  return arr.sort((a,b) => (a[field] > b[field]) ? 1 : ((b[field] > a[field]) ? -1 : 0)); 
-}
