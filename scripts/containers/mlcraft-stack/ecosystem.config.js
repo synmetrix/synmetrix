b7f83fd70a8cbf80228c986dee40e28c48da88f4
Ivan Fokeev <vanya@vanya.cc>
2021-09-24 00:33:18 +0300

    fixes

--- scripts/containers/mlcraft-stack/ecosystem.config.js
+++ scripts/containers/mlcraft-stack/ecosystem.config.js
@@ -1,6 +1,36 @@
 module.exports = {
-  apps : [{
-    name   : 'client',
-    script : 'cd mlcraft/services/client && yarn install && yarn start'
-  }]
-}
+  apps : [
+    {
+      name   : 'client',
+      script : 'cd mlcraft/services/client && yarn install && yarn run build && cd build && npx serve'
+    },
+    {
+      name   : 'hasura',
+      script : './graphql-engine serve'
+    },
+    {
+      name   : 'hasura_migrations',
+      script : 'cd mlcraft/services/hasura && hasura migrate apply'
+    },
+    /*
+    {
+      name   : 'hasura_metadata',
+      script : 'cd mlcraft/services/hasura && hasura metadata apply'
+    },
+    */
+    {
+      name   : 'hasura_plus',
+      script : 'cd hasura-backend-plus && yarn install && yarn run build && yarn start'
+    },
+    /*
+    {
+      name   : 'actions',
+      script : 'cd mlcraft/services/actions && yarn install && yarn start'
+    },
+    {
+      name   : 'cubejs',
+      script : 'cd mlcraft/services/cubejs && yarn install && yarn start'
+    },
+    */
+  ]
+};
