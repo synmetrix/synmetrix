7643eef5627b97b3a341f081920b0c1ee8a61221
Ivan Fokeev <vanya@vanya.cc>
2023-05-16 19:45:07 +0500

    fix(cli): run swarm via docker-compose for new versions

--- cli/utils.py
+++ cli/utils.py
@@ -14,63 +14,72 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-PROJECT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+PROJECT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
 
 console = Console()
+
 
 def read_yaml(filename):
     if not os.path.exists(filename):
         return None
 
-    with open(filename, 'r') as stream:
+    with open(filename, "r") as stream:
         return load(stream, Loader=Loader)
 
+
 def write_yaml(data, filename):
-    with open(filename, 'a') as stream:
+    with open(filename, "a") as stream:
         yaml_string = dump(data, Dumper=Dumper)
         stream.write(yaml_string)
         return yaml_string
 
+
 def console_yellow(text):
-    console.log(f'[yellow]{text}[/yellow]')
+    console.log(f"[yellow]{text}[/yellow]")
+
 
 def console_red(text):
-    console.log(f'[red]{text}[/red]')
+    console.log(f"[red]{text}[/red]")
+
 
 def call_system(cmd):
-    console.log(Panel(f'Command: {cmd}'))
+    console.log(Panel(f"Command: {cmd}"))
 
-    code = os.system(f'/bin/bash -c {pipes.quote(cmd)}')
+    code = os.system(f"/bin/bash -c {pipes.quote(cmd)}")
 
     if code != 0:
         raise SystemExit
 
+
 def call_docker(ctx, cmd):
-    call_command = f'docker {cmd}'
+    call_command = f"docker {cmd}"
     call_system(call_command)
+
 
 def call_service(ctx, cmd):
-    call_command = f'docker service {cmd}'
+    call_command = f"docker service {cmd}"
     call_system(call_command)
 
+
 def call_compose(ctx, cmd):
-    args = [
-        '-f', 'docker-compose.yml',
-        '-f', ctx.obj['docker_compose_file']
-    ]
+    args = ["-f", "docker-compose.yml", "-f", ctx.obj["docker_compose_file"]]
 
     call_command = f'docker-compose {" ".join(args)} {cmd}'
     call_system(call_command)
 
+
 def call_swarm(ctx, cmd):
-    compose_args = [
-        '-f', 'docker-compose.yml',
-        '-f', ctx.obj['docker_compose_file']
-    ]
+    compose_args = " ".join(
+        ["-f", "docker-compose.yml", "-f", ctx.obj["docker_compose_file"]]
+    )
 
-    args = [
-        '-c', f'<(docker-compose {" ".join(compose_args)} config)',
-    ]
+    args = " ".join(
+        [
+            "-c",
+            '<(echo -e "version: \'3.9\'"; docker-compose %s config| (sed "/published:/s/\\"//g") | (sed "/^name:/d"))'
+            % compose_args,
+        ]
+    )
 
-    call_command = f'docker stack {cmd} {" ".join(args)}'
+    call_command = "docker stack %s %s" % (cmd, args)
     call_system(call_command)
--- create-overlay-network.sh
+++ create-overlay-network.sh
@@ -1,1 +1,2 @@
+docker network rm mlcraft_default
 docker network create --driver=overlay --attachable mlcraft_default
