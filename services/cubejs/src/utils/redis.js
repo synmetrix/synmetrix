0d3b24392013bdf28bd42f2555bb23a453116e4b
Nikita <gorland1314@list.ru>
2023-06-30 18:59:43 +0300

    pervert spam logs

--- services/cubejs/src/utils/redis.js
+++ services/cubejs/src/utils/redis.js
@@ -6,6 +6,11 @@ let redisClient = null;
 
 if (REDIS_ADDR) {
   redisClient = new Redis(REDIS_ADDR);
+
+  redisClient.on('error', (error) => {
+    if (error?.code === 'ECONNREFUSED' || error?.code === 'EAI_AGAIN') return;
+    console.error(error);
+  })
 }
 
 export default redisClient;
