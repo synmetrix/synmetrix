282f369ed9189a13a72d926496c4824be6bb58b9
Ivan Fokeev <vanya@vanya.cc>
2023-07-31 14:26:23 +0300

    fix: reports

--- services/actions/src/rpc/sendExplorationScreenshot.js
+++ services/actions/src/rpc/sendExplorationScreenshot.js
@@ -89,10 +89,12 @@ const getDataAndScreenshot = async (exploration) => {
   const { datasource_id: datasourceId, user_id: userId, id: explorationId } = exploration;
 
   const explorationTableURL = `${APP_FRONTEND_URL}/~/explore/${datasourceId}/${explorationId}/?screenshot=1`;
+  console.log("explorationTableURL", explorationTableURL)
 
   const accessToken = await generateUserAccessToken(userId);
 
   if (!accessToken) {
+    console.error('Access token hasn`t been signed')
     return { error: 'Access token hasn`t been signed' };
   }
 
