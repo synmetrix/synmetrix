158c3a4fd9804c5b30ab04e531e0807627ce9240
Nikita <gorland1314@list.ru>
2023-08-04 16:57:05 +0300

    check browser locale

--- services/client/src/components/LanguageSelect.js
+++ services/client/src/components/LanguageSelect.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocalStorageState } from 'ahooks';
+import { DEFAULT_LANGUAGE } from '../i18n';
 
 import ru from 'flag-icons/flags/4x3/ru.svg';
 import us from 'flag-icons/flags/4x3/us.svg';
@@ -11,14 +12,27 @@ import s from './LanguageSelect.module.css';
 
 const { Option } = Select;
 
+const availableLangs = {
+  en: us,
+  ru,
+};
+
+const getLanguage = () => {
+  const userLang = navigator.language || navigator.userLanguage;
+  const langs = Object.keys(availableLangs);
+
+  let setLang = langs.find(lang => userLang.includes(lang));
+
+  if (!setLang) {
+    setLang = DEFAULT_LANGUAGE;
+  }
+
+  return setLang;
+};
+
 const LanguageSelect = () => {
   const { i18n } = useTranslation();
-  const [state, setState] = useLocalStorageState('locale', 'en');
-
-  const options = [
-    { value: 'en', icon: us },
-    { value: 'ru', icon: ru },
-  ];
+  const [state, setState] = useLocalStorageState('locale', getLanguage());
 
   const handleChange = useCallback((lang) => {
     i18n.changeLanguage(lang);
@@ -38,12 +52,12 @@ const LanguageSelect = () => {
       className={s.select}
       dropdownClassName={s.dropdown}
     >
-      {options.map(v => (
-        <Option key={v.value} value={v.value}>
+      {Object.entries(availableLangs).map(([lang, icon]) => (
+        <Option key={lang} value={lang}>
           <img
             alt=''
             className={s.image}
-            src={v.icon}
+            src={icon}
           />
         </Option>
       ))}
--- services/client/src/i18n.js
+++ services/client/src/i18n.js
@@ -4,11 +4,13 @@ import locales from '../locales/locales';
 import moment from 'moment';
 import 'moment/min/locales';
 
+export const DEFAULT_LANGUAGE = 'en';
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: locales,
-    lng: 'en',
+    lng: DEFAULT_LANGUAGE,
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
