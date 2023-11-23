83f4aaf1fedaa7dd4cc0f1f397e5d64f2aadf1ce
Ivan Fokeev <vanya@vanya.cc>
2023-11-23 10:31:54 +0700

    feat(member_roles): add created_at, updated_at columns

---
+++ services/hasura/migrations/1700709743404_alter_table_public_member_roles_add_column_updated_at/up.sql
@@ -0,0 +1,19 @@
+alter table "public"."member_roles" add column "updated_at" timestamptz
+ null default now();
+
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_member_roles_updated_at"
+BEFORE UPDATE ON "public"."member_roles"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_member_roles_updated_at" ON "public"."member_roles" 
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
