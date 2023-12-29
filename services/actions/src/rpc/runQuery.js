ea46e08ad32d38d05e700eb1246d1bd4cf670ef1
Nikita <gorland1314@list.ru>
2023-12-29 15:08:10 +0300

    feat(runQuery): disable for members

--- services/actions/src/rpc/runQuery.js
+++ services/actions/src/rpc/runQuery.js
@@ -1,11 +1,35 @@
 import cubejsApi from '../utils/cubejsApi';
 import apiError from '../utils/apiError';
+import { fetchGraphQL } from '../utils/graphql';
+
+const roleQuery = `
+  query ($user_id: uuid!, $datasource_id: uuid!) {
+    member_roles(where: {member: {_and: {user_id: {_eq: $user_id}, team: {datasources: {id: {_eq: $datasource_id}}}}}}) {
+      team_role
+    }
+  }
+`;
 
 export default async (session, input, headers) => {
   const { datasource_id: dataSourceId, query, limit } = input || {};
   const userId = session?.['x-hasura-user-id'];
+  const authToken = headers?.authorization;
 
   try {
+    const roleResult = await fetchGraphQL(
+      roleQuery,
+      {
+        user_id: userId,
+        datasource_id: dataSourceId,
+      },
+      authToken
+    );
+
+    const role = roleResult?.data?.member_roles?.[0]?.team_role;
+    if (!['owner', 'admin'].includes(role)) {
+      return apiError('No permissions');
+    }
+
     const result = await cubejsApi({
       dataSourceId,
       userId,
