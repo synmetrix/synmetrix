a4576bb4303440faeea2743af6dddcbe2d866086
Ivan Fokeev <vanya@vanya.cc>
2024-02-15 23:00:56 +0300

    fix: bash scripts

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-export HASURA_GRAPHQL_ADMIN_SECRET=devsecret
+export HASURA_GRAPHQL_ADMIN_SECRET=${HASURA_GRAPHQL_ADMIN_SECRET:-"devsecret"}
 
 ./cli.sh compose up --init
 ./migrate.sh
--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,5 @@
+export HASURA_GRAPHQL_ADMIN_SECRET=${HASURA_GRAPHQL_ADMIN_SECRET:-"devsecret"}
+
 ./cli.sh hasura cli "migrate apply --all-databases" --build
 ./cli.sh hasura cli "seeds apply --all-databases"
 
